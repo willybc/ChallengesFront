fc467e204d44518595fba11e517b4d6c48666cc1
Willy Bardales <willybc06@gmail.com>
2020-12-28 00:20:55 -0300

    add new component

---
+++ src/app/profilecard/profilecard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profilecard',
+  templateUrl: './profilecard.component.html',
+  styleUrls: ['./profilecard.component.sass']
+})
+export class ProfilecardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
