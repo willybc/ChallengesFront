3baadeb247c2fda1fc49664e5e826c28671be4d6
Willy Bardales <willybc06@gmail.com>
2020-12-29 19:04:00 -0300

    proyect pod request

--- src/app/pod-request/pod-request.component.ts
+++ src/app/pod-request/pod-request.component.ts
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PodRequestComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    document.body.style.backgroundColor = "rgb(17, 23, 37)";
+    
+    document.body.style.alignItems = "center";
+    document.body.style.fontFamily = "Chivo, sans-serif";
+    document.body.style.fontSize = "18px";
+    document.body.style.color = "white";
+    document.body.style.height = "100vh";
+    document.body.style.width = "100vw";
+    document.body.style.overflow = "hidden";
+  }
 
   ngOnInit(): void {
   }
